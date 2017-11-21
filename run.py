8fb56f6dccb513e6d17a85647206f07a84d39171
Yuqiwu <ywu@stuy.edu>
2017-11-21 14:45:30 -0500

    added search for youtube

--- run.py
+++ run.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, session, redirect, url_for, request, flash
-
-app = Flask(__name__)
-
+    
 @app.route('/')
 def route_root():
     return render_template('home.html')
 
 @app.route('/about')
 def route_about():
+    "type = request.form["type"]
+    search = request.form["search"]
+    if type == 'google'
+    u = urllib2.urlopen('http://www.freegeoip.net/json/%s' % website)
+    data_string = u.read()
+    d = json.loads(data_string)"
     return render_template('about.html')
 
 if __name__ == "__main__":
