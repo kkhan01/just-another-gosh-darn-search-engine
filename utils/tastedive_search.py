651c071a422479c81141f290133ee4aaa0bd41e0
soe43 <wsoe@stuy.edu>
2017-11-28 00:16:31 -0500

    added string for key input

--- utils/tastedive_search.py
+++ utils/tastedive_search.py
@@ -1,4 +1,4 @@
 from flask import Flask, render_template, session, redirect, url_for, request, flash
 
-def tastedive_search(search):
-    return "https://tastedive.com/api/similar?q=%s" % search
+def tastedive_search(search, key):
+    return "https://tastedive.com/api/similar?k=%s&?q=%s" %(key,search)
