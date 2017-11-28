105b824492dc05152002ebf919123deeee574a2c
Yuqiwu <ywu@stuy.edu>
2017-11-27 19:14:49 -0500

    updated google and news search methods

--- utils/google_search.py
+++ utils/google_search.py
@@ -4,4 +4,10 @@ def google_search(search):
     with open ('api_keys.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         key = reader['Google']
-    return "http://www.google.com/customsearch/v1?key=%s&cx=001172072688314740745:fwbcfymikgm&q=%s" % (key, search)
+    dictionary = "http://www.google.com/customsearch/v1?key=%s&cx=001172072688314740745:fwbcfymikgm&q=%s" % (key, search)
+    ret = []
+    for thing in dictionary:
+        items = result['items']
+        sub = [items['title'], items['snippet'], items['link']]
+        ret.append(sub)
+    return ret
--- utils/news_search.py
+++ utils/news_search.py
@@ -4,4 +4,9 @@ def news_search(search):
     with open ('api_keys.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         key = reader['News']
-    return "https://newsapi.org/v2/top-headlines?apiKey=%s&sources=%s" % (key, search)
+    dictionary = "https://newsapi.org/v2/top-headlines?apiKey=%s&sources=%s" % (key, search)
+    ret = []
+    for news in dictionary:
+        sub = [news['title'], news['description'], news['url']]
+        ret.append(sub)
+    return ret
