84e8cb81399367215189381f7bd624fd977d85e8
soe43 <wsoe@stuy.edu>
2017-11-28 21:32:14 -0500

    pushing test file

---
+++ utils/test.py
@@ -0,0 +1,7 @@
+from tastedive import tastedive_search
+from news import news_search
+from google import google_search
+
+print tastedive_search('Harry Potter')
+print news_search('bitcoin')
+print google_search('cats')
