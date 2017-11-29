6be64e34f57a8fbcfe880a65514ff9fe65b2427a
Yuqiwu <ywu@stuy.edu>
2017-11-28 21:49:46 -0500

    updated google and youtube

--- utils/google.py
+++ utils/google.py
@@ -1,18 +1,21 @@
 import csv
-import requests
 
-def search(search):
+def google_search(search):
     with open ('api_keys.csv') as csvfile:
         reader = csv.DictReader(csvfile)
-        key = reader['Google']
-    api_url = "http://www.google.com/customsearch/v1?"
-    payload = { 'key' : key,
-                'cx' : '001172072688314740745:fwbcfymikgm',
-                'q' : search}
-    dictionary = requests.get(api_url, params=payload)
-    ret = []
-    for thing in dictionary:
-        items = result['items']
-        sub = [items['title'], items['snippet'], items['link']]
-        ret.append(sub)
-    return ret
+        reader.next()
+        for line in reader:
+            if line['Type'] == 'Google':
+                key = line.get('API_KEY')
+        api_url = "http://www.google.com/customsearch/v1?"
+        payload = { 'key' : key,
+                    'cx' : '001172072688314740745:fwbcfymikgm',
+                    'q' : search}
+        r = requests.get(api_url, params=payload)
+        r = r.json()
+        ret = []
+        for thing in r:
+            items = result['items']
+            sub = [items['title'], items['snippet'], items['link']]
+            ret.append(sub)
+        return ret
--- utils/youtube.py
+++ utils/youtube.py
@@ -1,40 +1,24 @@
-import urllib
-import jinja2
 import csv
 
-from apiclient.discovery import build
-from optparse import OptionParser
-
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'])
-
-# Set DEVELOPER_KEY to the "API key" value from the Google Developers Console:
-# https://console.developers.google.com/project/_/apiui/credential
-# Please ensure that you have enabled the YouTube Data API for your project.
-with open ('api_keys.csv') as csvfile:
+def youtube_search(search):
+    with open ('api_keys.csv') as csvfile:
         reader = csv.DictReader(csvfile)
-        DEVELOPER_KEY = reader['Youtube']
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-app = Flask(__name__)
-
-def search(search):
-    youtube = build(
-        YOUTUBE_API_SERVICE_NAME, 
-        YOUTUBE_API_VERSION, 
-        developerKey=DEVELOPER_KEY)
-    search_response = youtube.search().list(
-        q="Hello",
-        part="id,snippet",
-        maxResults=5
-        ).execute()
-
-    videos = []
-    
-    for search_result in search_response.get("items", []):
-        if search_result["id"]["kind"] == "youtube#video":
-            videos.append("%s (%s)" % (search_result["snippet"]["title"], 
-            search_result["id"]["videoId"]))
-
-    return videos
+        reader.next()
+        for line in reader:
+            if line['Type'] == 'Youtube':
+                key = line.get('API_KEY')
+        api_url = "https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.channels.list?"
+        payload = { 'part' : 'snippet',
+                    'q' : search,
+                    'type' : 'video',
+                    'videoCaption' : 'closedCaption',
+                    'key' = key}
+        result = requests.get(api_url, params=payload)
+        result = result.json()
+        ret = []
+        for thing in result:
+                link = 'https://www.youtube.com/watch?v=' + items['id']['videoId']
+                items = result['items']['snippet']
+                sub = [items['title'], items['description'], link]
+                ret.append(sub)
+        return ret
