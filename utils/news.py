a565546e70c186fb4510105edca36769a9f3751f
soe43 <wsoe@stuy.edu>
2017-11-28 20:58:14 -0500

    Completed search functionality for news and tastedive apis

--- utils/news.py
+++ utils/news.py
@@ -1,11 +1,17 @@
 import requests
 import csv
+import codecs
 
-def search(search):
-    with open ('api_keys.csv') as csvfile:
-        reader = csv.DictReader(csvfile)
-        key = reader['News']
+def news_search(search):
+    reader = csv.DictReader(codecs.open('api_keys.csv','rU', 'utf-16'))
+    for row in reader:
+        search_type = row['Type']
+        key = row.get('API')
+        if search_type == 'News':
+            use_key = key
+    print use_key
     api_url = "https://newsapi.org/v2/everything"
-    payload = {'key' : key, 'q' : search}
+    payload = {'apikey' : use_key, 'q' : search}
     r = requests.get(api_url, params = payload)
+    print r.url
     return r.json()
--- utils/tastedive.py
+++ utils/tastedive.py
@@ -1,16 +1,17 @@
 import csv
 import requests
+import codecs
 
-def search(search):
-    with open ('api_keys.csv') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for line in reader: 
-            search_type = line['Type']
-            key = line.get('API_KEY')
-            if search_type == 'TasteDive':
-                use_key = key
-        api_url = "https://tastedive.com/api/similar"
-        payload = {'k' : use_key, 'q' : search}
-        r = requests.get(api_url, params = payload, verify = False)
-        return r.json()
+def tastedive_search(search):
+    reader = csv.DictReader(codecs.open('api_keys.csv','rU', 'utf-16'))
+    for row in reader:
+        search_type = row['Type']
+        key = row.get('API')
+        if search_type == 'TasteDive':
+            use_key = key
+    api_url = "https://tastedive.com/api/similar"
+    payload = {'k' : use_key, 'q' : search}
+    r = requests.get(api_url, params = payload)
+    return r.json()
 
+    
