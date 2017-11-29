8b23f6bc1ca17c804df100303201c2ad034409b8
soe43 <wsoe@stuy.edu>
2017-11-28 19:58:49 -0500

    moving files to git bash

--- utils/tastedive.py
+++ utils/tastedive.py
@@ -1,11 +1,16 @@
 import csv
+import requests
 
 def search(search):
     with open ('api_keys.csv') as csvfile:
         reader = csv.DictReader(csvfile)
-        key = reader['TasteDive']
+        for line in reader: 
+            search_type = line['Type']
+            key = line.get('API_KEY')
+            if search_type == 'TasteDive':
+                use_key = key
         api_url = "https://tastedive.com/api/similar"
-        payload = {'k' : key, 'q' : search}
-        r = requests.get(api_url, params = payload)
+        payload = {'k' : use_key, 'q' : search}
+        r = requests.get(api_url, params = payload, verify = False)
         return r.json()
 
