fec85fc99121053d08ac3ffde8ec49df36b9e3e2
soe43 <wsoe@stuy.edu>
2017-11-28 21:54:10 -0500

    google workaround

--- utils/google.py
+++ utils/google.py
@@ -1,18 +1,23 @@
 import csv
 import requests
+import codecs
 
-def search(search):
-    with open ('api_keys.csv') as csvfile:
-        reader = csv.DictReader(csvfile)
-        key = reader['Google']
-    api_url = "http://www.google.com/customsearch/v1?"
-    payload = { 'key' : key,
+def google_search(search):
+    reader = csv.DictReader(codecs.open('api_keys.csv','rU', 'utf-16'))
+    for row in reader:
+        search_type = row['Type']
+        key = row.get('API')
+        if search_type == 'Google':
+            use_key = key
+    api_url = "http://www.google.com/customsearch/v1"
+    payload = { 'key' : use_key,
                 'cx' : '001172072688314740745:fwbcfymikgm',
                 'q' : search}
-    dictionary = requests.get(api_url, params=payload)
-    ret = []
+    r = requests.get(api_url, params=payload)
+    return r.json()
+    '''ret = []
     for thing in dictionary:
         items = result['items']
         sub = [items['title'], items['snippet'], items['link']]
         ret.append(sub)
-    return ret
+    return ret'''
